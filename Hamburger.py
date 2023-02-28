3a65988802737b3caa24aebefd3c43203e06848a
dwright343 <126611540+dwright343@users.noreply.github.com>
2023-02-28 12:26:06 -0700

    Add files via upload

---
+++ Hamburger.py
@@ -0,0 +1,13 @@
+# IS 303 Group Project
+
+from random import randint
+
+class Order():
+    def __init__(self):
+        self.burger_count = ""
+    def randomBurgers(self):
+        randomBurgers = randint(1,20)
+
+class Person():
+    def __init__(self):
+        self.
